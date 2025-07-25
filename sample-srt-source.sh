dcecd74e8c9059594faae440e07ec6270552c525
adrianroe <roe.adrian@gmail.com>
2025-07-25 16:40:21 +0100

    Sample source does not assume wget

--- sample-srt-source.sh
+++ sample-srt-source.sh
@@ -5,6 +5,15 @@ cd "${0%/*}"
 SOURCES_DIR=$(readlink -f ./sources)
 
 
+if command -v curl > /dev/null 2>&1; then
+    fetch() { curl  --silent --fail -L "$1" -o "$2"; }
+    elif command -v wget > /dev/null 2>&1; then
+    fetch() { wget --quiet "$1" -O "$2"; }
+else
+    oops "you don't have wget or curl installed, which I need to download the binary tarball"
+fi
+
+
 usage() {
     echo "Usage: ${0##*/} <camera1|camera2> <start|stop>"
     echo "  Start a sample SRT source that sends a stream to camera 1 or camera 2 on port 5001"
@@ -16,7 +25,7 @@ function downloadMediaAsset() {
     local -r target="$SOURCES_DIR/$fileName"
     if [[ ! -f $target ]]; then
         echo "Downloading sample file $target"
-        wget -O "$target.download" "https://s3.eu-west-1.amazonaws.com/norsk.video/media-examples/data/$fileName"
+        fetch "https://s3.eu-west-1.amazonaws.com/norsk.video/media-examples/data/$fileName" "$target.download"
         mv "$target.download" "$target"
     fi
 }
