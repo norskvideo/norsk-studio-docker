6c4b6717223557f35952234d936b8e5cee7632d7
Fabrizio Ferrai <fabrizio.ferrai@gmail.com>
2025-09-08 11:00:55 +0300

    Detect Oracle Linux too

--- deployed/detect.sh
+++ deployed/detect.sh
@@ -3,6 +3,8 @@ if [[ -f /sys/class/dmi/id/bios_vendor && "$(cat /sys/class/dmi/id/bios_vendor)"
   echo "Google"
 elif [[ -f /sys/class/dmi/id/bios_vendor && "$(cat /sys/class/dmi/id/bios_vendor)" = "Linode" ]]; then
   echo "Linode"
+elif [[ -f /var/norsk-studio/norsk-studio-docker/deployed/vendor && "$(cat /var/norsk-studio/norsk-studio-docker/deployed/vendor)" = "Oracle" ]]; then
+  echo "Oracle"
 else
   echo "local"
 fi
