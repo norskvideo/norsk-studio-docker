cfc85f3afe52e646da2a2641397d292b5fca10f6
Verity Scheel <monoidmusician@gmail.com>
2025-09-10 12:13:30 -0500

    New version

--- up.sh
+++ up.sh
@@ -2,8 +2,8 @@
 set -eo pipefail
 cd "${0%/*}"
 
-export NORSK_MEDIA_IMAGE=norskvideo/norsk:1.0.402-2025-09-08-0b83f9cf
-export NORSK_STUDIO_IMAGE=norskvideo/norsk-studio:1.0.402-2025-09-08-0b83f9cf
+export NORSK_MEDIA_IMAGE=norskvideo/norsk:1.0.402-2025-09-10-38401717
+export NORSK_STUDIO_IMAGE=norskvideo/norsk-studio:1.0.402-2025-09-10-38401717
 
 declare NETWORK_MODE_DEFAULT
 declare LOCAL_TURN_DEFAULT
