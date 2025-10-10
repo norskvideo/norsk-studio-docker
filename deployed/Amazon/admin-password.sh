7ea1d77bdf24d8b68f5116f4d67bfb3bfe601db7
Verity Scheel <monoidmusician@gmail.com>
2025-10-10 12:05:42 -0500

    Admin password for Amazon

---
+++ deployed/Amazon/admin-password.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+TOKEN=`curl -s -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600"`
+curl -s -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/meta-data/instance-id || echo "$(hostname)-norsk-studio-password"
