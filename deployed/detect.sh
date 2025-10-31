6c01afbb028804c146a287354a5c7060da5c9450
Verity Scheel <monoidmusician@gmail.com>
2025-10-31 08:59:54 -0500

    Better detection for Amazon EC2

--- deployed/detect.sh
+++ deployed/detect.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
-if [[ -f /sys/class/dmi/id/bios_vendor && "$(cat /sys/class/dmi/id/bios_vendor)" = "Google" ]]; then
+if [[ -f /var/norsk-studio/norsk-studio-docker/deployed/vendor ]]; then
+  cat /var/norsk-studio/norsk-studio-docker/deployed/vendor
+elif [[ -f /sys/class/dmi/id/bios_vendor && "$(cat /sys/class/dmi/id/bios_vendor)" = "Google" ]]; then
   echo "Google"
 elif [[ -f /sys/class/dmi/id/bios_vendor && "$(cat /sys/class/dmi/id/bios_vendor)" = "Linode" ]]; then
   echo "Linode"
-elif [[ -f /sys/class/dmi/id/bios_vendor && "$(cat /sys/class/dmi/id/bios_vendor)" = "Amazon EC2" ]]; then
+elif [[ -f /sys/class/dmi/id/bios_vendor && "$(cat /sys/class/dmi/id/bios_vendor)" = "Amazon EC2" || -f /sys/class/dmi/id/bios_version && "$(cat /sys/class/dmi/id/bios_version)" =~ "amazon" ]]; then
   echo "Amazon"
-elif [[ -f /var/norsk-studio/norsk-studio-docker/deployed/vendor && "$(cat /var/norsk-studio/norsk-studio-docker/deployed/vendor)" = "Oracle" ]]; then
-  echo "Oracle"
 else
   echo "local"
 fi
