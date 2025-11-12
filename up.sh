0a6a475ec056b90837a2bb0e61d2c8ace4c68b8a
Jankings <jankings@id3as.co.uk>
2025-11-12 14:06:24 +0000

    Update to Studio 1.27.0-2025-11-12-2555c434 with Norsk SDK 1.0.402-2025-11-12-4b26aa2e

--- up.sh
+++ up.sh
@@ -6,8 +6,8 @@ export NORSK_MEDIA_IMAGE=norskvideo/norsk:1.0.402-2025-09-10-38401717
 export NORSK_STUDIO_IMAGE=norskvideo/norsk-studio:1.0.402-2025-09-10-38401717
 
 # Nightly builds
-export NIGHTLY_NORSK_MEDIA_IMAGE=norskvideo/norsk:null
-export NIGHTLY_NORSK_STUDIO_IMAGE=norskvideo/norsk-studio:1.27.0-2025-11-12-d15cae7a
+export NIGHTLY_NORSK_MEDIA_IMAGE=norskvideo/norsk:1.0.402-2025-11-12-4b26aa2e
+export NIGHTLY_NORSK_STUDIO_IMAGE=norskvideo/norsk-studio:1.27.0-2025-11-12-2555c434
 
 declare NETWORK_MODE_DEFAULT
 declare LOCAL_TURN_DEFAULT
