3b823f7449491fa19efd3a1af800999ab7132722
adrianroe <roe.adrian@gmail.com>
2025-12-01 19:51:06 +0000

    fix typo

--- deployed/norsk-containers.sh
+++ deployed/norsk-containers.sh
@@ -67,7 +67,7 @@ args=(
     --network-mode host
     --public-url "${PUBLIC_URL_PREFIX:-https://$DEPLOY_HOSTNAME/norsk}"
     --studio-url "${STUDIO_URL_PREFIX:-https://$DEPLOY_HOSTNAME/studio}"
-    --turn=true
+    --turn "true"
 )
 
 if [[ -n "$ice_servers" ]]; then
